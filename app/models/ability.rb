4012eda7feffb414678d33da9a95c2df61bc0fc5
Amsterdam AL <amsterdamAL@gmail.com>
2019-10-26 10:01:31 -0400

    heroku error troubleshoot

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -28,7 +28,7 @@ class Ability
  #
             # Define abilities for the passed in user here. For example:
             #
-            ies for the passed in user here. For example:
+            
     #
     #   user ||= User.new # guest user (not logged in)
     #   if user.admin?
